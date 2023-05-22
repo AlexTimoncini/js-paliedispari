784ba3744a129091925efbabecccec8037db1f10
AlexTimoncini <alextimonci@gmail.com>
2023-05-22 18:18:28 +0200

    reset + score calculator

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ let paliButtonDom = document.getElementById('paliButton');
 let wordOutputDom = document.getElementById('words_output');
 let userWord = document.getElementById('userWord');
 let wordList = [];
+let score = 0;
+let scoreDom = document.getElementById('score');
+
 paliButtonDom.addEventListener('click', function(){
     if (paliChecker(userWord.value) && !alreadyTakenChecker(wordList, userWord.value)){
         let liElementDom = document.createElement('li');
@@ -12,7 +15,9 @@ paliButtonDom.addEventListener('click', function(){
         wordOutputDom.appendChild(liElementDom);
         userWord.classList.add('right');
         userWord.classList.remove('wrong');
-        wordList.push(userWord.value)
+        wordList.push(userWord.value);
+        score += parseInt(scoreCalc(userWord.value));
+        scoreDom.innerHTML = score;
     } else if (!paliChecker(userWord.value)){
         console.log('this word is not palindrome');
         userWord.classList.remove('right');
@@ -22,7 +27,14 @@ paliButtonDom.addEventListener('click', function(){
         userWord.classList.remove('right');
         userWord.classList.add('wrong');
     }
+    userWord.value = '';
 });
+
+function scoreCalc(rigthWord){
+    let finalScore = 0;
+    finalScore += (rigthWord.length * 1000) + 100;
+    return finalScore;
+}
 
 function paliChecker(randomWord){
     let splitWord = [];
