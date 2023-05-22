3deb4d644c42ce587c6776b1e2a9fe05cccc0025
AlexTimoncini <alextimonci@gmail.com>
2023-05-22 14:53:04 +0200

    added style and linked pali function to button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+let userWord;
+let paliButtonDom = document.getElementById('paliButton');
+paliButtonDom.addEventListener('click', function(){
+    userWord = document.getElementById('userWord').value;
+    if (paliChecker(userWord)){
+        console.log('this word is plaindrome');
+    } else {
+        console.log('this word is not plaindrome');
+    }
+});
 
-let userWord = prompt('Inserisci una parola per verificare se sia palindroma o meno');
-
-console.log(paliChecker(userWord));
 
 function paliChecker(randomWord){
     let splitWord = [];
